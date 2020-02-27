15d78a6130b44517983a111a5c5a999b873a8fa0
Kevin Deisz <kevin.deisz@gmail.com>
2020-02-27 12:34:49 -0500

    Technically more accurate types

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,7 +1,7 @@
 export type BracketSets = typeof import("./bracketSets.json");
 export type BracketSetsKey = keyof BracketSets;
 
-export type BracketSet = BracketSets extends ({ [key: string]: (infer U) }) ? U : never;
+export type BracketSet = BracketSets extends ({ [K in BracketSetsKey]: (infer U) }) ? U : never;
 export type Bracket = BracketSet extends (infer U)[] ? U : never;
 
 export type FilingType = "single" | "joint" | "head";
