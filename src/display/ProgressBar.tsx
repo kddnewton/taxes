1e596abb1b9889c8f6741376f471b6c2f139c340
Kevin Deisz <kevin.deisz@gmail.com>
2020-03-05 17:14:38 -0500

    More style fixes

--- src/display/ProgressBar.tsx
+++ src/display/ProgressBar.tsx
@@ -6,15 +6,22 @@ type ProgressBarProps = {
   value: number;
 };
 
-const ProgressBar: React.FC<ProgressBarProps> = ({ value }) => (
-  <div
-    className={styles.progress}
-    style={{ width: `${value}%` }}
-    role="progressbar"
-    aria-valuenow={value}
-    aria-valuemin={0}
-    aria-valuemax={100}
-  />
-);
+const ProgressBar: React.FC<ProgressBarProps> = ({ value }) => {
+  const style = {
+    opacity: (value / 200) + 0.5,
+    width: `${value}%`
+  };
+
+  return (
+    <div
+      className={styles.progress}
+      style={style}
+      role="progressbar"
+      aria-valuenow={value}
+      aria-valuemin={0}
+      aria-valuemax={100}
+    />
+  );
+};
 
 export default ProgressBar;
