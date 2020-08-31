448e09e743153ab59b25812aac514ade3ff99160
Kevin Deisz <kevin.deisz@gmail.com>
2020-08-31 13:30:42 -0400

    Bump deps

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,6 +1,7 @@
 export type BracketSets = typeof import("./bracketSets.json");
 export type BracketSetsKey = keyof BracketSets;
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export type BracketSet = BracketSets extends ({ [K in BracketSetsKey]: (infer U) }) ? U : never;
 export type Bracket = BracketSet extends (infer U)[] ? U : never;
 
